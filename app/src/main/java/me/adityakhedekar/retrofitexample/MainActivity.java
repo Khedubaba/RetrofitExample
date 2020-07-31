2c65ba9adb4fd9e18c25a1acc76ad4a05371fa00
khedubaba <adityakhedekar49@gmail.com>
2020-07-31 19:20:48 +0530

    Get JSON data from REST API using Retrofit and converting JSON into Java Object using Gson before displaying on screen.
    Signed-off-by: khedubaba <adityakhedekar49@gmail.com>

--- app/src/main/java/me/adityakhedekar/retrofitexample/MainActivity.java
+++ app/src/main/java/me/adityakhedekar/retrofitexample/MainActivity.java
@@ -3,12 +3,68 @@ package me.adityakhedekar.retrofitexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewRresult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewRresult = findViewById(R.id.text_view_result);
+
+        //instance of Retrofit
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        //now with Retrofit instance above we can create JSON Placeholder API
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+
+        //Now to execute network request we have to use Call object we declared in JsonPlaceHolderApi Interface class
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
+
+        /*to execute call will lead to run async task on main thread thus we need to do this on background thread but
+        we don't have to do this by ourselves, retrofit takes care of this by convenience method call enqueue*/
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()) {
+                    textViewRresult.setText("Code: " + response.code());
+                    return;
+                }
+
+                List<Post> posts = response.body();
+
+                for (Post post : posts) {
+                    String content = "";
+                    content += "Id: " + post.getId() + "\n";
+                    content += "User ID: " + post.getUserId() + "\n";
+                    content += "Title: " + post.getTitle() + "\n";
+                    content += "Text: " + post.getText() + "\n\n";
+
+                    //use .append instead of .setText to not to overrite previous content
+                    textViewRresult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewRresult.setText(t.getMessage());
+            }
+        });
+
     }
 }
